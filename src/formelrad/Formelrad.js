38092dc977ae8f970093bdcbb502dd7aed7463b4
Philip Baumgartner <deine.email@example.com>
2025-06-11 10:13:41 +0200

    Apply patch 0012: Power is also required

--- src/formelrad/Formelrad.js
+++ src/formelrad/Formelrad.js
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
 import {useState} from "react";
-=======
 import '../css/mvp.css';
->>>>>>> featureSimpleStyling
 import formelrad from "../image/formelradelektronik.gif";
 import InputField from "../formular/InputField";
 
@@ -10,40 +7,24 @@ export default function Formelrad() {
     const [values, setValues] = useState({
         u: 10,
         i: 2,
-        r: ""
+        r: "",
+        p: ""
     })
-
     return (
         <>
-<<<<<<< HEAD
-            <h2>Formelrad</h2>
-            <img src={formelrad} width="200" alt="Formelrad"/>
-            <form>
-                <InputField color={"black"} value={values.u} label="Spannung" handleChange={e => {setValues(values => ({...values, u: e.target.value}))}} />
-                <InputField color={"black"} value={values.i} label="Stromstaerke" handleChange={e => {setValues(values => ({...values, i: e.target.value}))}} />
-                <InputField color={"black"} value={values.r} label="Widerstand" handleChange={e => {setValues(values => ({...values, r: e.target.value}))}} />
-                <button type="submit">Calculate</button>
-            </form>
-=======
             <section>
                 <header>
                     <h2>Formelrad</h2>
                     <img src={formelrad} width="200" alt="Formelrad"/>
                 </header>
                 <form>
-                    <div>
-                        <label>Spannung</label><input></input>
-                    </div>
-                    <div>
-                        <label>Strom</label><input></input>
-                    </div>
-                    <div>
-                        <label>Widerstand</label><input></input>
-                    </div>
+                    <InputField color={"black"} value={values.u} label="Spannung" handleChange={e => {setValues(values => ({...values, u: e.target.value}))}} />
+                    <InputField color={"black"} value={values.i} label="Stromstärke" handleChange={e => {setValues(values => ({...values, i: e.target.value}))}} />
+                    <InputField color={"black"} value={values.r} label="Widerstand" handleChange={e => {setValues(values => ({...values, r: e.target.value}))}} />
+                    <InputField color={"black"} value={values.p} label="Leistung" handleChange={e => {setValues(values => ({...values, p: e.target.value}))}} />
                     <button type="submit">Calculate</button>
                 </form>
             </section>
->>>>>>> featureSimpleStyling
         </>
     )
 }
